cbf4fd239b7bebe610bd5dcdb28e89031632fea3
Vineeth J <vineeth.jk@gmail.com>
2021-02-23 19:02:25 +0530

    Created all the functions and program is fully functional.

--- MovieCollectionApp.py
+++ MovieCollectionApp.py
@@ -1,19 +1,26 @@
+movie_list = []
+movie_dict = {}
+user_input = None
+
 def addMovie(name,director,year):
-    pass
+    movie_dict = {'Name': name, 'Director': director, 'Year': year}
+    movie_list.append(movie_dict)
 
 def listMovies():
-    pass
+    for movie in movie_list:
+        print(movie)
 
 def findMovie(movieName):
-    pass
+    for movie in movie_list:
+        if movie['Name'] == movieName:
+            print(movie)
 
 
 print("""Enter one of the following options.
-         1. Adding a movie to your collection
-         2. Listing all the movies in your collection
-         3. Finding a movie in your collection 
-         q. Qutting the program\n""")
-
+            1. Adding a movie to your collection
+            2. Listing all the movies in your collection
+            3. Finding a movie in your collection 
+            q. Qutting the program\n""")
 user_input = input("Please enter one of the above options: ").lower()
 
 while user_input != 'q':
@@ -22,20 +29,39 @@ while user_input != 'q':
             movie_director = input("Please enter the movie director name: ").lower()
             movie_year = input("Please enter the year when the movie was released: ").lower()
             addMovie(movie_name, movie_director, movie_year)
-            break
+            print("""\nEnter one of the following options.
+            1. Adding a movie to your collection
+            2. Listing all the movies in your collection
+            3. Finding a movie in your collection 
+            q. Qutting the program\n""")
+            user_input = input("Please enter one of the above options: ").lower()
+            continue
         elif user_input == '2':
             listMovies()
-            break
+            print("""\nEnter one of the following options.
+            1. Adding a movie to your collection
+            2. Listing all the movies in your collection
+            3. Finding a movie in your collection 
+            q. Qutting the program\n""")
+            user_input = input("Please enter one of the above options: ").lower()
+            continue
         elif user_input == '3':
             find_movie = input("Please enter the movie name you want to find: ").lower()
             findMovie(find_movie)
-            break
-        else:
-            print("""Please enter '1' for adding a movie, 
-            '2' for listing all the movies,
-            '3' for finding a movie, 
-            or 'q' for quitting the program.""")
+            print("""\nEnter one of the following options.
+            1. Adding a movie to your collection
+            2. Listing all the movies in your collection
+            3. Finding a movie in your collection 
+            q. Qutting the program\n""")
             user_input = input("Please enter one of the above options: ").lower()
+            continue
+        else:
+           print("""\nEnter one of the following options.
+            1. Adding a movie to your collection
+            2. Listing all the movies in your collection
+            3. Finding a movie in your collection 
+            q. Qutting the program\n""")
+           user_input = input("Please enter one of the above options: ").lower()
 
         
 
